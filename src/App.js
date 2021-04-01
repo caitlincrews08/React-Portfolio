25ccbee7648944b6263bd5b6a549abcbfc388152
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-01 13:35:49 -0400

    setup app routes and imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
+import Wrapper from "./components/Wrapper/wrapper";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Projects from "./pages/projects";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path = "/" component={About}/>
+          <Route exact path = "/about" component={About}/>
+          <Route exact path = "/contact" component={Contact}/>
+          <Route exact path = "/projects" component={Projects}/>
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
