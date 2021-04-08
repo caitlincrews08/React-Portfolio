75e1b24f980d6a7c4e314697b5129f3d0b4be2e3
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-08 16:47:58 -0400

    modified css

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-// import "./style.css";
+import "./style.css";
 
 function NavComponent() {
 
-  return (
-      <nav>
-          <Navbar bg="dark" variant="dark">
-              <Navbar.Brand href="home" >Caitlin Crews</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="mr-auto">
-                      <Nav.Link href="about">About</Nav.Link>
-                      <Nav.Link href="contact">Contact</Nav.Link>
-                      <Nav.Link href="portfolio">Portfolio</Nav.Link>
-                  </Nav>
-              </Navbar.Collapse>
-          </Navbar>
-      </nav>
-  );
+    return (
+        <nav>
+            <Navbar variant="dark" sticky="top" className="custom">
+                <Navbar.Brand href="home" >Caitlin Crews</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="about">About Me</Nav.Link>
+                        <Nav.Link href="portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="resume">Resume</Nav.Link>
+                        <Nav.Link href="contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            
+        </nav>
+    );
 };
 
 export default NavComponent;
