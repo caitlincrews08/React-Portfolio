152942637153aa88014a6068d249fca9d0f57804
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-13 19:08:12 -0400

    included all contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,20 @@
+import React from "react";
+import {  Card, Button } from "react-bootstrap";
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { MdEmail } from "react-icons/md";
+
 function Contact() {
-  return(
-    <div>
-      <p>Contact stuff here</p>
+  return (
+    <div className="contact">
+      <h2>Contact information</h2>
+      <h3>Caitlin Crews</h3>
+      <div className="links">
+        <p><AiFillGithub />Github: <a href="https://github.com/caitlincrews08">github.com/caitlincrews08</a> </p>
+        <p><AiFillLinkedin />Linkedin: <a href="https://linkedin.com/in/caitlin-crews">linkedin.com/in/caitlin-crews</a></p>
+        <p><MdEmail />Email: <a href="mailto:caitlinrcrews@gmail.com">caitlinrcrews@gmail.com</a></p>
+      </div>
+
+      {/* Spruce up later */}
     </div>
   )
 
