fac9ec9be38c3de2ea143f7d662666fa17838086
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-13 20:07:21 -0400

    setting up layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,24 @@
-function About() {
-  return(
-    <div>
-      <p>About stuff here</p>
-    </div>
-  )
-}
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import gradpic from "../assets/images/gradpic.png"
 
+function About() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          {/* <Col><img src={gradpic}/></Col> */}
+          <Col><h2>Full Stack Web Developer</h2></Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col><h2>Fullstack Web Developer</h2></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  )   
+}
+ 
 export default About;
