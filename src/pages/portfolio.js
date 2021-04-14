5e7e8f02988fa5a3cdca7dfac36014e96c7922ca
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-14 01:44:07 -0400

    created base template - half complete

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,44 +1,90 @@
 import React from "react";
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck, Button } from 'react-bootstrap';
+import { AiFillGithub } from 'react-icons/ai'
+
+import travelers from '../assets/travelersTop5.png';
+import meme from '../assets/memeheim.png';
+import budget from '../assets/budget_screenshot.png';
 
 function Portfolio() {
   return (
-    <div>
+    <div className="container">
+      <CardDeck className="Cards">
+        <Card style={{ height: '100%' }}>
+          <Card.Img variant="top" src={meme} />
+          <Card.Body>
+            <Card.Title>Memeheim</Card.Title>
+            <Card.Text>
+            Create, save, and find memes.
+            </Card.Text>
+            <Button href="https://memeheim.herokuapp.com">Deployment</Button>
+            <Button href="https://github.com/caitlincrews08/Final-Project">Github</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Backend Developer</small>
+          </Card.Footer>
+        </Card>
+        <Card style={{ height: '100%' }}>
+          <Card.Img variant="top" src={travelers} />
+          <Card.Body>
+            <Card.Title>Traveler's Top Five</Card.Title>
+            <Card.Text>
+            Find the best rated restaurants, hotels, and sights in a given city.
+            </Card.Text>
+            <Button href="https://caitlincrews08.github.io/Travelers-Top-5/">Deployment</Button>
+            <Button href="https://github.com/caitlincrews08/Travelers-Top-5">Github</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Backend Developer</small>
+          </Card.Footer>
+        </Card>
+        <Card style={{ height: '100%' }}>
+          <Card.Img variant="top" src={budget} />
+          <Card.Body>
+            <Card.Title>Budget Tracker</Card.Title>
+            <Card.Text>
+            Budget tracker with online and offline capability.
+           </Card.Text>
+           <Button href="https://stormy-gorge-52058.herokuapp.com">Deployment</Button>
+            <Button href="https://github.com/caitlincrews08/Budget-Tracker">Github</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">PWA functionality</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+      <br></br>
       <CardDeck>
         <Card>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Memeheim</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-        </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-          content.{' '}
+            Create, save, and find memes.
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">Backend Developer</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={travelers} />
+          <Card.Body>
+            <Card.Title>Traveler's Top Five</Card.Title>
+            <Card.Text>
+            Find the best rated restaurants, hotels, and sights in a given city.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Backend Developer</small>
           </Card.Footer>
         </Card>
         <Card>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Budget Tracker</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
+            Budget tracker with online and offline capability.
         </Card.Text>
           </Card.Body>
           <Card.Footer>
