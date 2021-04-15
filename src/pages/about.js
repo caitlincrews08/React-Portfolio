e3232b3926b5a624891562ad25f05a492705157f
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-14 23:46:24 -0400

    deleted grad image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import gradpic from "../assets/images/gradpic.png"
+
 
 function About() {
   return (
     <div>
       <Container>
         <Row>
-          {/* <Col><img src={gradpic}/></Col> */}
           <Col><h2>Full Stack Web Developer</h2></Col>
           <Col></Col>
         </Row>
