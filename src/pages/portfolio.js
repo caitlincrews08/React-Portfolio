c86106fc6a19faa1552e6f9d785f927add32812c
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-15 00:11:41 -0400

    complete - update for simplicity later

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Card, CardDeck, Button } from 'react-bootstrap';
-import { AiFillGithub } from 'react-icons/ai'
+// import { AiFillGithub } from 'react-icons/ai'
 
 import travelers from '../assets/travelersTop5.png';
 import meme from '../assets/memeheim.png';
 import budget from '../assets/budget_screenshot.png';
+import team from '../assets/teambuilder.png';
+import employee from '../assets/employeedirect.png';
+import fitness from '../assets/fitness.png';
 
 function Portfolio() {
   return (
     <div className="container">
       <CardDeck className="Cards">
-        <Card style={{ height: '100%' }}>
+        <Card>
           <Card.Img variant="top" src={meme} />
           <Card.Body>
             <Card.Title>Memeheim</Card.Title>
             <Card.Text>
-            Create, save, and find memes.
+            Create, save, and find memes
             </Card.Text>
             <Button href="https://memeheim.herokuapp.com">Deployment</Button>
             <Button href="https://github.com/caitlincrews08/Final-Project">Github</Button>
@@ -24,29 +27,29 @@ function Portfolio() {
             <small className="text-muted">Backend Developer</small>
           </Card.Footer>
         </Card>
-        <Card style={{ height: '100%' }}>
+        <Card>
           <Card.Img variant="top" src={travelers} />
           <Card.Body>
             <Card.Title>Traveler's Top Five</Card.Title>
             <Card.Text>
-            Find the best rated restaurants, hotels, and sights in a given city.
+            Find the best rated restaurants, hotels, and sights in a given city
             </Card.Text>
             <Button href="https://caitlincrews08.github.io/Travelers-Top-5/">Deployment</Button>
             <Button href="https://github.com/caitlincrews08/Travelers-Top-5">Github</Button>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Backend Developer</small>
+            <small className="text-muted">JavaScript</small>
           </Card.Footer>
         </Card>
-        <Card style={{ height: '100%' }}>
+        <Card>
           <Card.Img variant="top" src={budget} />
           <Card.Body>
             <Card.Title>Budget Tracker</Card.Title>
             <Card.Text>
-            Budget tracker with online and offline capability.
+            Budget tracker with online and offline capability
            </Card.Text>
            <Button href="https://stormy-gorge-52058.herokuapp.com">Deployment</Button>
-            <Button href="https://github.com/caitlincrews08/Budget-Tracker">Github</Button>
+           <Button href="https://github.com/caitlincrews08/Budget-Tracker">Github</Button>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">PWA functionality</small>
@@ -56,39 +59,44 @@ function Portfolio() {
       <br></br>
       <CardDeck>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={team} />
           <Card.Body>
-            <Card.Title>Memeheim</Card.Title>
+            <Card.Title>Employee Template Engine</Card.Title>
             <Card.Text>
-            Create, save, and find memes.
+            Employees' information at a glance
             </Card.Text>
+           <Button href="https://github.com/caitlincrews08/Employee-Template-Engine">Github</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Node CLI</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={fitness} />
+          <Card.Body>
+            <Card.Title>Workout Tracker</Card.Title>
+            <Card.Text>
+            Track your workout regimen and view stats
+            </Card.Text>
+            <Button href="https://desolate-plains-06411.herokuapp.com/?id=6077bb492403f10015ed2d37">Deployment</Button>
+           <Button href="https://github.com/caitlincrews08/Workout-Tracker">Github</Button>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Backend Developer</small>
           </Card.Footer>
         </Card>
         <Card>
-          <Card.Img variant="top" src={travelers} />
+          <Card.Img variant="top" src={employee} />
           <Card.Body>
-            <Card.Title>Traveler's Top Five</Card.Title>
+            <Card.Title>Employee Directory</Card.Title>
             <Card.Text>
-            Find the best rated restaurants, hotels, and sights in a given city.
-            </Card.Text>
+            Quickly search through an employee database
+           </Card.Text>
+           <Button href="https://caitlincrews08.github.io/Employee-Directory">Deployment</Button>
+           <Button href="https://github.com/caitlincrews08/Employee-Directory">Github</Button>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Backend Developer</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Budget Tracker</Card.Title>
-            <Card.Text>
-            Budget tracker with online and offline capability.
-        </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">React</small>
           </Card.Footer>
         </Card>
       </CardDeck>
@@ -97,3 +105,5 @@ function Portfolio() {
 }
 
 export default Portfolio;
+
+// reeeally need to update this later
