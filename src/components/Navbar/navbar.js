d6e5d27828d9dd9a8eb8bf28ca82090720c2a31e
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-20 18:19:17 -0400

    set up hashrouter

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -7,13 +7,13 @@ function NavComponent() {
     return (
         <nav>
             <Navbar variant="dark" sticky="top" className="custom">
-                <Navbar.Brand href="home" >Caitlin Crews</Navbar.Brand>
+                <Navbar.Brand href="#home" >Caitlin Crews</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="about">About Me</Nav.Link>
-                        <Nav.Link href="portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="contact">Contact</Nav.Link>
+                        <Nav.Link href="#about">About Me</Nav.Link>
+                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
