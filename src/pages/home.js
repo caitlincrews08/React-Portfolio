76519463f068515c99618fd34e33d7756b4aaf7b
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-20 16:44:24 -0400

    updated intro page with button and added technologies

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Jumbotron, Button } from "react-bootstrap";
 
 function Home() {
   return (
-    <div className="bkg">
-        <Container fluid>
-          <h1>Hey!</h1>
-          <p>
-            Thanks for checking out my portfolio.
-          </p>
-        </Container>
-    </div>
+    <Jumbotron className="home text-center">
+      <div className="center">
+      <h1>Hi, I'm Caitlin.</h1>
+      <h5>
+        I'm a full-stack web developer based in Orlando, Florida.
+      </h5>
+      <p className="bold">HTML/CSS | JavaScript | Bootstrap | React | NodeJS | Express | MySQL | MongoDB</p>
+      <p>
+        <Button variant="primary" href="about">Learn more</Button>
+      </p>
+      </div>
+    </Jumbotron>
   )
 }
 
