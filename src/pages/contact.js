a39c995e590ff8800661145239331ee90d206cc5
Caitlin Crews <caitlinrcrews@gmail.com>
2021-04-20 17:18:00 -0400

    modified css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {  Button } from "react-bootstrap";
+import {  Button, Jumbotron } from "react-bootstrap";
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { MdEmail } from "react-icons/md";
 
 function Contact() {
   return (
-    <div className="contact">
-      <h2>Contact information</h2>
-      <h3>Caitlin Crews</h3>
+    <Jumbotron className="contact home text-center">
+      <h2>Contact Me</h2>
+      <hr/>
+      <h4>Caitlin Crews</h4>
       <div className="links">
         <p><AiFillGithub />Github: <a href="https://github.com/caitlincrews08">github.com/caitlincrews08</a> </p>
         <p><AiFillLinkedin />Linkedin: <a href="https://linkedin.com/in/caitlin-crews">linkedin.com/in/caitlin-crews</a></p>
@@ -16,7 +17,7 @@ function Contact() {
       </div>
 
       {/* Spruce up later */}
-    </div>
+    </Jumbotron>
   )
 
 }
